7e5c70cc6fb9dbbc5f2ebc529a80e4eb01a52a6e
warrenskim <42902241+warrenskim@users.noreply.github.com>
2021-08-04 17:31:17 -0700

    Updates

---
+++ webapp.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+from wikiscraper import flagscraper, search_formatter, wiki_url, wikiscraper, formatted_wikiscraper
+import os
+
+app = Flask(__name__)
+
+# TAKE THIS OUT WHEN DEPLOYING
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+
+# Display homepage
+@app.route("/")
+def get_home():
+    return render_template('index.html')
+
+
+@app.route("/comparison", methods=['GET', 'POST'])
+def get_comparison():
+    state1_name = request.args.get('state1')
+    state2_name = request.args.get('state2')
+
+    state1_search = search_formatter(state1_name)
+    state2_search = search_formatter(state2_name)
+
+    url1 = wiki_url(state1_search)
+    url2 = wiki_url(state2_search)
+
+    scraped1 = wikiscraper(url1)
+    scraped2 = wikiscraper(url2)
+
+    flag1 = flagscraper(url1)
+    flag2 = flagscraper(url2)
+
+    return render_template('comparison.html', state1_name=state1_name, state2_name=state2_name, 
+    state1_data=scraped1, state2_data=scraped2, flag1=flag1, flag2=flag2)
+
+
+# Need to make a request for this --> If Python use requests.get(url for search) or if Node url parser
+# How do they send the request from their language
+@app.route("/search", methods=['GET', 'POST'])
+def get_search():
+    search_name = request.args.get('search')
+
+    search_formatted = search_formatter(search_name)
+
+    url = wiki_url(search_formatted)
+
+    scraped = formatted_wikiscraper(url)
+    return jsonify(scraped)
+
+
+@app.route("/test", methods=['GET', 'POST'])
+def get_test():
+    Object = requests.get("http://127.0.0.1:3157/search?search=Smith+Rock+state+park")
+    Object1 = Object.text
+
+    return render_template('test.html', Object1=Object1)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 3157)) 
+    app.run(port=port, debug=True) 
---
+++ wikiscraper.py
@@ -0,0 +1,64 @@
+import string
+import requests
+from bs4 import BeautifulSoup
+
+def search_formatter(search):
+    search_input = string.capwords(search)
+    input_list = search_input.split()
+    search_word = "_".join(input_list)
+
+    if search_input == "Georgia":
+        search_word += "_(U.S._state)"
+    if search_input == "New York":
+        search_word += " (state)"
+
+    return search_word
+
+
+def wiki_url(search_word):
+    url = "https://en.wikipedia.org/wiki/"+search_word
+    return url
+
+
+def wikiscraper(url):
+    url_open = requests.get(url)
+    soup = BeautifulSoup(url_open.content, 'html.parser')
+
+    labels = soup.find_all(class_='infobox-label')
+    datas = soup.find_all(class_='infobox-data')
+
+    results = dict()
+
+    for x,y in zip(labels, datas):
+        key = x.text
+        value = y.text
+        results[key] = value
+        
+    return results
+
+
+def formatted_wikiscraper(url):
+    url_open = requests.get(url)
+    soup = BeautifulSoup(url_open.content, 'html.parser')
+
+    labels = soup.find_all(class_='infobox-label')
+    datas = soup.find_all(class_='infobox-data')
+
+    results = dict()
+
+    for x,y in zip(labels, datas):
+        key = x.text.encode('ascii', 'ignore').decode("utf-8")
+        value = y.text.encode('ascii', 'ignore').decode("utf-8")
+        results[key] = value
+        
+    return results
+
+
+def flagscraper(url):
+    url_open = requests.get(url)
+    soup = BeautifulSoup(url_open.content, 'html.parser')
+
+    image = soup.find("meta", property="og:image")
+    image_url = image.get("content", None)
+        
+    return image_url
